1589b8e35107b1d2f1dc8357b494ff3d29c4ac4f
mintpeo <tranquocminh15981@gmail.com>
2026-07-09 11:48:29 +0700

    Update Info

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Portfolio from "./mainPage/portfolio/Portfolio.jsx";
 import './utils/language/i18n.js'
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
-
   return (
     <>
-      <Router>
-          <Routes>
-              <Route path="/" element={<Portfolio />}></Route>
-          </Routes>
-      </Router>
+        <Analytics />
+        <Router>
+            <Routes>
+                <Route path="/" element={<Portfolio />}></Route>
+            </Routes>
+        </Router>
     </>
   )
 }
--- src/mainPage/info/Info.jsx
+++ src/mainPage/info/Info.jsx
@@ -1,10 +1,11 @@
-import React from "react"
+import React, {useState} from "react"
 import './Info.css'
 import {useTranslation} from "react-i18next";
 
 export default function Info() {
     const {t, i18n} = useTranslation();
     const lang = i18n.language;
+    const [isImageModalOpen, setIsImageModalOpen] = useState(false);
 
     const infoData = {
         title: {
@@ -16,8 +17,12 @@ export default function Info() {
             "vi": "Thực tập sinh Full-stack"
         },
         hero: {
-          "en": "Passionate about web application development and always eager to learn new technologies. I enjoy solving problems, writing clean code, and continuously improving my skills. My goal is to contribute to real-world projects while gaining valuable experience and growing into a professional software developer.",
-          "vi": "Đam mê phát triển ứng dụng web và luôn sẵn sàng học hỏi những công nghệ mới. Tôi yêu thích giải quyết vấn đề, viết mã nguồn sạch và không ngừng nâng cao kỹ năng của bản thân. Mục tiêu của tôi là đóng góp vào các dự án thực tế đồng thời tích lũy kinh nghiệm để phát triển trở thành một lập trình viên chuyên nghiệp.",
+          "en": "Passionate about building web applications and always eager to learn new technologies. Enjoy solving problems, writing clean code, and continuously improving my skills. My goal is to gain hands-on experience and grow into a professional software developer.",
+          "vi": "Thích xây dựng các ứng dụng web và luôn sẵn sàng học hỏi những công nghệ mới. Yêu thích giải quyết vấn đề, viết mã nguồn sạch và không ngừng nâng cao kỹ năng. Mục tiêu là tích lũy kinh nghiệm để phát triển trở thành một lập trình viên chuyên nghiệp.",
+        },
+        chatbox: {
+            "en": "Always committed to doing my best.",
+            "vi": "Sẽ luôn cố gắng trong công việc.",
         }
     }
 
@@ -31,7 +36,24 @@ export default function Info() {
                     </h1>
 
                     <p className="hero-subtitle">{infoData.hero[lang]}</p>
-                    <a href="#projects" className="hero-btn">Creator journey</a>
+                    <div className="hero-avatar-container">
+                        {/* Chat Box */}
+                        <div className="avatar-chatbox">
+                            <span>{infoData.chatbox[lang]}</span>
+                            <br/>
+                            <br/>
+                            <p style={{textAlign: "center"}}>{t("portfolio:img-btn")}!</p>
+                            <div className="chatbox-arrow"></div> {/* Mũi tên nhỏ chĩa xuống dưới */}
+                        </div>
+
+                        <img
+                            src="src/assets/cv_head.jpg"
+                            alt="Tran Quoc Minh Avatar"
+                            className="hero-avatar"
+                            onClick={() => setIsImageModalOpen(true)}
+                        />
+                    </div>
+                    {/*<a href="#projects" className="hero-btn">Creator journey</a>*/}
                 </div>
 
                 {/* Cột mốc Timeline cố định bên phải của Hero */}
@@ -43,6 +65,16 @@ export default function Info() {
                     <div className="timeline-dot"></div>
                 </div>
             </div>
+
+            {/*  Modal  */}
+            {isImageModalOpen && (
+                <div className="image-modal-overlay" onClick={() => setIsImageModalOpen(false)}>
+                    <div className="image-modal-content" onClick={(e) => e.stopPropagation()}>
+                        <button className="image-modal-close" onClick={() => setIsImageModalOpen(false)}>✕</button>
+                        <img src="src/assets/cv_full.jpg" alt="Tran Quoc Minh Avatar" className="enlarged-avatar" />
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
